1e68b0a6d9be88a0bc458ae7c5ff0fae6bb902d7
colbyfayock <fayock@gmail.com>
2021-01-30 16:21:38 -0500

    favicon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -36,7 +36,6 @@ const Layout = ({ children }) => {
 
         <meta name="description" content={metaDescription} />
 
-        {/* Favicon sizes and manifest generated via https://favicon.io/ */}
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
