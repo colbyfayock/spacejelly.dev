009dda751d77416ade7f4260bcd69f26aa8b7a83
colbyfayock <fayock@gmail.com>
2021-02-08 22:07:54 -0500

    gtag formatting

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,19 +1,25 @@
 export const GA_TRACKING_ID = 'G-9LR59CQX33';
 
-// @via https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/lib/gtag.js
+/**
+ * pageview
+ * @via https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+ */
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
+export function pageview(url) {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
-};
+}
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
+/**
+ * event
+ * @via https://developers.google.com/analytics/devguides/collection/gtagjs/events
+ */
+
+export function event({ action, category, label, value }) {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
   });
-};
+}
