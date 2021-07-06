dda2ba938eed8efb9884ecd4124ad49ad1b8d8dc
colbyfayock <fayock@gmail.com>
2021-07-06 11:14:38 -0400

    better error handling for parsing url params

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -97,8 +97,16 @@ export function testNodeInHeaders(headers) {
  */
 
 export function getUrlParamsFromString(string) {
-  const url = new URL(string);
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (e) {
+    throw new Error(`Failed to get URL params from string: ${e.message}`);
+  }
+
   const params = new URLSearchParams(url.search);
+
   return Array.from(params.keys()).map((key) => {
     return {
       key,
