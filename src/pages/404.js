6383bc31898819c49a652f77a80a828cbc6b7658
colbyfayock <fayock@gmail.com>
2021-09-15 20:16:51 -0400

    shrug on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,6 +8,7 @@ import Header from 'components/Header';
 import Content from 'components/Content';
 import Section from 'components/Section';
 import Container from 'components/Container';
+import CosmoShrug from 'components/CosmoShrug';
 
 import styles from 'styles/pages/Page.module.scss';
 
@@ -31,10 +32,13 @@ export default function Store({ courses }) {
       <Content>
         <Section className={styles.pageSection}>
           <Container className={styles.pageContent}>
+            <p className="text-center">
+              <CosmoShrug />
+            </p>
             <p className="text-center">Not sure what you were looking for, but this isn't it!</p>
             <p className="text-center">
               <Link href="/">
-                <a>Go back home!</a>
+                <a>Go Back Home</a>
               </Link>
             </p>
           </Container>
